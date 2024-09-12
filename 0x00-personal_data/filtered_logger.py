9dcd4a32905d39772804af6f5f6df06b06e5c846
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-09-13 00:31:29 +0100

    resolving requirement issue

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,5 +9,5 @@ from typing import List
 def filter_datum(fields: List[str], redaction: str, message: str, seperator: str) -> str:
     """ return the logs message obfuscated """
     pattern = r'({})([^{}]+)'.format("|".join(fields), seperator)
-    repls = r'\1={}'.format(redaction)
+    repls = lambda m: "{}={}".format(m.group(1), redaction)
     return re.sub(pattern, repls, message)
