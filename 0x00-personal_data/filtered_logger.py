2941f8cacfaa983ee2fb1197eba918255b7db852
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-09-13 00:27:14 +0100

    annotate the filter_datum function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,9 +4,9 @@
 contains a function that filters a message
 """
 import re
+from typing import List
 
-
-def filter_datum(fields, redaction, message, seperator):
+def filter_datum(fields: List[str], redaction: str, message: str, seperator: str) -> str:
     """ return the logs message obfuscated """
     pattern = r'({})([^{}]+)'.format("|".join(fields), seperator)
     repls = r'\1={}'.format(redaction)
