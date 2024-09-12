6b5628ad649a9fcd28d22325effd28efdd0e5cad
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-09-13 00:33:34 +0100

    resolve the pycodestyle issue

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,8 +6,12 @@ contains a function that filters a message
 import re
 from typing import List
 
-def filter_datum(fields: List[str], redaction: str, message: str, seperator: str) -> str:
+
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 seperator: str) -> str:
     """ return the logs message obfuscated """
     pattern = r'({})([^{}]+)'.format("|".join(fields), seperator)
-    repls = lambda m: "{}={}".format(m.group(1), redaction)
+    repls = r'\1={}'.format(redaction)
     return re.sub(pattern, repls, message)
