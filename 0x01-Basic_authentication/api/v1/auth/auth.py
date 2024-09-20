3fe7a038116179a14b2136b59cc8e0293076cfaf
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-09-20 15:56:45 +0100

    documented the require_auth method in the Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,6 +12,9 @@ class Auth:
     """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        returns True if the path is not in the list of strings excluded_path
+        """
         if path is None:
             return True
         if excluded_paths is None or len(excluded_paths) == 0:
@@ -37,4 +40,7 @@ class Auth:
         return auth_header
 
     def current_user(self, request=None) -> TypeVar("User"):
+        """
+        return the current user logged in user
+        """
         return None
